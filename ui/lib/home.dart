66e1824403f3d24dba9ac5f65b616c31888e2a69
akhsak <akhsamariyam2k2@gmail.com>
2024-05-03 11:58:21 +0530

    add

--- ui/lib/home.dart
+++ ui/lib/home.dart
@@ -5,36 +5,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
         padding: const EdgeInsets.all(25),
         child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Center(
-                  child: Text(
-                'Epic Games:An American Vedio Game And Software Devoloper',
+              Text(
+                'Epic Games:An American Vedio Game And Software Devoloper And Publisher Based in Cary,North Caronia',
                 style: TextStyle(color: Colors.white),
-              )),
+              ),
               const Text(
                 'And Publisher Based in Cary,North Caronia',
                 style: TextStyle(color: Colors.white),
               ),
               const SizedBox(height: 20),
-              const Image(image: AssetImage('ui/assets/download (1).jpg')),
+              const Image(
+                image: AssetImage(
+                  'assets/download (1).jpg',
+                ),
+                width: double.infinity,
+              ),
               const Text(
                 'Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one thats right for you and your friends. Find it all in Fortnite Drop In.',
                 style: TextStyle(fontSize: 8, color: Colors.white),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 5,
               ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Visit Website',
-                  style: TextStyle(color: Colors.black),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Visit Website',
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
               ),
               const SizedBox(
@@ -42,33 +52,54 @@ class HomePage extends StatelessWidget {
               ),
               const SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Image(
-                      image: AssetImage(
-                        'ui/assets/Screenshot ui.png',
+                child: Center(
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Image(
+                            image: AssetImage(
+                              'assets/Screenshot ui.png',
+                            ),
+                            height: 200,
+                            width: 80,
+                          ),
+                          Text(
+                            'Explore Large,',
+                            style: TextStyle(
+                                fontSize: 7,
+                                color: Colors.white,
+                                fontWeight: FontWeight.normal),
+                          ),
+                        ],
                       ),
-                      height: 100,
-                      width: 50,
-                    ),
-                    Image(
-                      image: AssetImage('ui/assets/Screenshot ui2.png'),
-                      height: 100,
-                      width: 10,
-                    ),
-                    Image(
-                      image: AssetImage('ui/assets/Screenshot ui3.png'),
-                      height: 100,
-                      width: 10,
-                    ),
-                  ],
+                      SizedBox(width: 5),
+                      Column(
+                        children: [
+                          Image(
+                            image: AssetImage('assets/Screenshot ui2.png'),
+                            height: 200,
+                            width: 80,
+                          ),
+                          Text('Discover Even More Way'),
+                        ],
+                      ),
+                      SizedBox(width: 5),
+                      Column(
+                        children: [
+                          Image(
+                            image: AssetImage('assets/Screenshot ui3.png'),
+                            height: 200,
+                            width: 80,
+                          ),
+                          Text('Team Up With Friends By Sprinting'),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const Text(
-                'Explore Large, Destructible'
-                ' Environments Where No Two Games '
-                'Are Ever The Same',
-                style: TextStyle(color: Colors.white, fontSize: 5),
               ),
               const SizedBox(
                 height: 10,
@@ -89,16 +120,64 @@ class HomePage extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              const Text(
-                '5M      500k    10X ',
-                style: TextStyle(color: Colors.white),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        '5M ',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Text(
+                        'Daily User Engagements',
+                        style: TextStyle(color: Colors.white, fontSize: 5),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        '\$500K',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Text(
+                        'Revenue Surge for an Platform',
+                        style: TextStyle(color: Colors.white, fontSize: 5),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        '10X',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Text(
+                        'ROAS on all our'
+                        'marketing campaigns',
+                        style: TextStyle(color: Colors.white, fontSize: 7),
+                      ),
+                    ],
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 30,
               ),
               const Text(
-                'Interested In Delving Deeper Into The Project?',style: TextStyle(color: Colors.white),),
-              Text(  'If youd like to explore further details about our initiatives or any of our affiliated brands, dont hesitate to connect. You can reach out to us via email at hello@abc.com or give us a call at +91 480 20802730.',
+                'Interested In Delving Deeper Into The Project?',
+                style: TextStyle(color: Colors.white),
+              ),
+              const Text(
+                'If youd like to explore further details about our initiatives or any of our affiliated brands, dont hesitate to connect. You can reach out to us via email at hello@abc.com or give us a call at +91 480 20802730.',
                 style: TextStyle(color: Colors.white),
               ),
             ],
@@ -108,3 +187,14 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+
+// Map<int,String>  a={
+//   10:'name',
+
+
+// };
+// {
+// 10:'akhsa';
+// }
